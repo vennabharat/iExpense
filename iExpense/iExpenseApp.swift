3c2f9bb9afaabec1f32c182378000b1a5b5d592d
Venna Bharat <v.bharath39@gmail.com>
2023-10-06 04:21:14 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by bharat venna on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
